8883e93e7b7666af0c07309dac846f4c1f962f23
Eric Charles <badass@stanford.edu>
2023-11-20 17:56:59 -0800

    Lots of delinting with pylint

--- tests/core/test_introspection.py
+++ tests/core/test_introspection.py
@@ -1,6 +1,4 @@
 import tempfile
-import pkgutil
-import setuptools
 
 import rail
 from rail.core import RailEnv
@@ -10,7 +8,7 @@ import rail.stages
 def test_print_rail_packages():
     RailEnv.print_rail_packages()
 
-    
+
 def test_print_rail_namespaces():
     RailEnv.print_rail_namespaces()
 
