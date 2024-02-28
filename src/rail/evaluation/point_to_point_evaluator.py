40fcf4a7def52a8fa7b5c00cf9317d964b457bee
Eric Charles <badass@stanford.edu>
2024-02-27 16:20:11 -0800

    added case where we force exact computation

--- src/rail/evaluation/point_to_point_evaluator.py
+++ src/rail/evaluation/point_to_point_evaluator.py
@@ -15,10 +15,12 @@ class PointToPointEvaluator(BaseEvaluator):
     name = 'PointToPointEvaluator'
     config_options = BaseEvaluator.config_options.copy()
     config_options.update(
+        hdf5_groupname=Param(str, "photometry", required=False,
+            msg="HDF5 Groupname for truth table."),        
         reference_dictionary_key=Param(str, "redshift", required=False,
             msg="The key in the `truth` dictionary where the redshift data is stored."),
-        point_estimate_key=Param(str, "redshift", required=False,
-            msg="The key in the `truth` dictionary where the redshift data is stored."),                        
+        point_estimate_key=Param(str, "zmode", required=False,
+            msg="The key in the point estimate table."),                        
     )
     inputs = [('input', QPHandle),
               ('truth', TableHandle)]
@@ -71,4 +73,41 @@ class PointToPointEvaluator(BaseEvaluator):
                 )
 
         self._output_table_chunk_data(start, end, out_table, first)
+
+        
+    def _process_all(self, data_tuple):
+
+        estimate_data = data_tuple[0]
+        reference_data = data_tuple[1][self.config.hdf5_groupname]
+        
+        out_table = {}
+        summary_table = {}
+        for metric in self.config.metrics:
+
+            if metric not in self._metric_dict:
+                #! Make the following a logged error instead of bailing out of the stage.
+                # raise ValueError(
+                print(f"Unsupported metric requested: '{metric}'.  Available metrics are: {self._metric_dict.keys()}")
+                continue
+
+            this_metric = self._metric_dict[metric](**self.config.get(metric, {}))
+
+            if this_metric.metric_output_type == MetricOutputType.single_value:
+                summary_table[metric] = np.array([this_metric.evaluate(
+                    np.squeeze(estimate_data.ancil[self.config.point_estimate_key]),
+                    reference_data[self.config.reference_dictionary_key],
+                )])
+
+            elif this_metric.metric_output_type == MetricOutputType.single_distribution:
+                print(f"{metric} with output type MetricOutputType.single_distribution not supported yet")
+                continue
+            else:
+                out_table[metric] = this_metric.evaluate(
+                    np.squeeze(estimate_data.ancil[self.config.point_estimate_key]),
+                    reference_data[self.config.reference_dictionary_key]
+                )
+
+        out_table_to_write = {key: np.array(val).astype(float) for key, val in out_table.items()}
+        self._output_handle = self.add_handle('output', data=out_table_to_write)
+        self._summary_handle = self.add_handle('summary', data=summary_table)
 
