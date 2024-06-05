53fed947e3c4b6909897d6efe83848bf75f86993
Eric Charles <echarles@gmail.com>
2024-06-05 11:05:10 -0700

    tweaks to stage

--- src/rail/core/stage.py
+++ src/rail/core/stage.py
@@ -93,7 +93,7 @@ class RailPipeline(MiniPipeline):
             return cls.pipeline_classes[name]
         except KeyError as msg:
             raise KeyError(f"Could not find pipeline class {name} in {list(cls.pipeline_classes.keys())}") from msg
-            
+
 
     @staticmethod
     def build_and_write(
@@ -118,7 +118,7 @@ class RailPipeline(MiniPipeline):
             None,
         )
         pipe.save(output_yaml)
-    
+
     def __init__(self):
         MiniPipeline.__init__(self, [], dict(name="mini"))
 
